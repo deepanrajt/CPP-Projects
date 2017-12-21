0f202ba4067a2cbddc9447b105a0fd13b8280053
deepanrajt <34570924+deepanrajt@users.noreply.github.com>
2017-12-21 10:53:55 +0530

    Create character printing for n times.cpp

---
+++ character printing for n times.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+char c;
+    unsigned int count,f;
+    f=1;
+    while (f=1) {
+        cin >> c >> count;
+        for (int i = 0; i < count; i++) {
+            cout << c;
+        }
+    }
+}
